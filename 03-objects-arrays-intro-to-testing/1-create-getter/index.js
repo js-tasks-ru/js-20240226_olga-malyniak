5ef7f0f65e452b1c7f9629f76add50064279b7cc
olga_malyniak <olga.malyniak.fe@gmail.com>
2024-03-25 17:25:09 +0200

    createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const keys = path.split('.');
+    
+    return function(obj){
+        for (let key of keys){
+            if(!obj?.hasOwnProperty(key)) return;
+            obj = obj[key];
+        }
+        return obj;
+    }
 }
