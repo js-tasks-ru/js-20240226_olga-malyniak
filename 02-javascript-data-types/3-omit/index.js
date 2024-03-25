08b9e8d9acd3287feae31f4d6e238ecd6ce4d003
olga_malyniak <olga.malyniak.fe@gmail.com>
2024-03-25 16:59:55 +0200

    omit - 2ой способ

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,10 +5,20 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    /** Первое решение
     const arrFromObj = Object.entries(obj);
     for (let field of fields){
      let key = arrFromObj.findIndex(item => item[0] === field);
      if (~key) arrFromObj.splice(key, 1);    
     }
     return Object.fromEntries(arrFromObj);
+    */
+   /** Второй способ */
+    const resObj = new Map(Object.entries(obj));
+    for (let key of resObj.keys()){
+        if (fields.includes(key)){
+           resObj.delete(key); 
+        }
+    }
+    return Object.fromEntries(resObj);
 };
