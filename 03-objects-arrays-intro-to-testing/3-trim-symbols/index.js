4b7f9d9e920ab6f71cb90d57dd8a0ff6f2ed0d8f
olga_malyniak <olga.malyniak.fe@gmail.com>
2024-03-26 15:39:21 +0200

    3-trim symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,18 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+    if (string === '' || size === 0) return '';
+    if (!size) return string;
+    let count = 0, result = '';
+    for(const char of string){
+        if(result[result.length-1] != char){
+            count=0;
+        }
+        if((result[result.length-1] == char && count < size) 
+            || result[result.length-1] != char){
+                result = result + char;
+                count++;           
+        }        
+    }
+    return result;
 }
