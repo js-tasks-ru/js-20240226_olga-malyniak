77df01d996f0b138e6b66411b62674b7949eda85
olga_malyniak <olga.malyniak.fe@gmail.com>
2024-03-30 20:16:43 +0200

    5.1. NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,46 @@
 export default class NotificationMessage {
+    static lastInstance;
+    classList = ['notification'];
+    constructor(message, {duration = 1000, type = ''} = {}){
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        this.element = this.createElement();
+    }
+    setFormatTime(){
+        return (this.duration / 1000).toFixed(0) + 's';
+    }
+    createElement(){
+        const element = document.createElement('div');
+        if (this.type.length) this.classList.push(this.type);
+        element.classList.add(...this.classList);
+        element.setAttribute('style', '--value:'+this.setFormatTime());
+        element.innerHTML = `
+                        <div class="timer"></div>
+                        <div class="inner-wrapper">
+                            <div class="notification-header">${this.type}</div>
+                            <div class="notification-body">${this.message}</div>
+                        </div>
+                `
+        return element;
+    }
+    show(container = document.body){
+        if (NotificationMessage.lastInstance) {
+            NotificationMessage.lastInstance.destroy();
+        }
+        NotificationMessage.lastInstance = this;
+        
+        this.timerId = setTimeout(() => {
+            this.destroy();
+        }, this.duration);
 
+        container.appendChild(this.element);
+    }
+    remove(){
+        this.element.remove();
+    }
+    destroy(){
+        this.remove();
+        clearTimeout(this.timerId);
+    }
 }
